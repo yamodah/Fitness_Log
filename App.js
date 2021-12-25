5f61521acbcfd19e60dd9ab4c7a90deb05a1ff42
yamodah <yassinedahlek@gmail.com>
2021-12-25 02:20:50 -0500

    builidng basic home page on app.js

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Yassine's Fitness Log</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.secondaryBG}>
+        <Text style={styles.text}>Yassine's Fitness Log</Text>
+        <StatusBar style="light" />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Log Wokrout</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "coral",
     alignItems: "center",
     justifyContent: "center",
+  },
+  secondaryBG: {
+    width: "85%",
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderRadius: 30,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: "azure",
+    borderRadius: 25,
+    padding: 10,
+  },
+  buttonText: {
+    color: "black",
   },
 });
