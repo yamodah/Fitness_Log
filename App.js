329acce7cee0ffc7dbb8c5c5f50f806c68e59fbf
yamodah <yassinedahlek@gmail.com>
2021-12-29 12:14:27 -0500

    added NavigationContainer

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from '@react-navigation/native';
 const Stack = createNativeStackNavigator()
 import Home from "./src/components/Home";
 
 export default function App() {
   return (
+    <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
       {/* <Stack.Screen name="GeneralInfo" component={Notifications} />
@@ -12,6 +14,7 @@ export default function App() {
       <Stack.Screen name="Sports" component={Settings} />
       <Stack.Screen name="Cardio" component={Settings} /> */}
     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
