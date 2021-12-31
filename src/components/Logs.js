047dd10ac353167fe76b0dc8bc581f53392e2f03
yamodah <yassinedahlek@gmail.com>
2021-12-31 17:29:25 -0500

    added logs function

---
+++ src/components/Logs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, } from 'react-native'
+
+const Logs = () => {
+    return (
+        <View>
+            <Text>Logs</Text>
+        </View>
+    )
+}
+
+export default Logs
+
+const styles = StyleSheet.create({})
