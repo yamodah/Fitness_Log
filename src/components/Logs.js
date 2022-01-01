60a80d6f8d93f463f1abf022906eae9eb4b1c5fa
yamodah <yassinedahlek@gmail.com>
2021-12-31 20:52:59 -0500

    extra '<' deleted

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -12,7 +12,7 @@ const Logs = ({logs}) => {
             <Text>workout goal:{item.goal}</Text>
             <Text>pre:{item.pre} post:{item.post} intensity:{item.intensity}</Text>
            {item.type ==="resistance" && <Text>body parts:{item.body_parts}</Text>}
-           {item.type==="sport" && <View><Text>sport:{item.sport} played at/in:{item.venue}</Text><<Text>competition:{item.competion}</Text></View>}
+           {item.type ==="sport" && <View><Text>sport:{item.sport} played at/in:{item.venue}</Text><Text>competition:{item.competion}</Text></View>}
            {item.type ==="cardio" && <Text>cardio type:{item.cardio_type} speed:{item.speed}</Text>}
         </View>
         )
