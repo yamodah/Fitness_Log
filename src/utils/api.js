ea520bced40d0a1b8258fad3b2bfe4b320503d8f
yamodah <yassinedahlek@gmail.com>
2021-12-31 20:32:30 -0500

    built out api routes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,27 +1,27 @@
 import axios from "axios"
-const URL = process.env.REACT_APP_BASE_API_URL
-require("dotenv").config()
+
+
 
 export async function listWorkouts(){
-    const data =  await axios.get(`${URL}`).catch(console.error)
+    const data =  await axios.get("https://yassine-fitness-api.herokuapp.com").catch(console.error)
     return data
 }
 export async function listWorkoutsByDate(workoutDate){
-    const data =  await axios.get(`${URL}/date/${workoutDate}`).catch(console.error)
+    const data =  await axios.get(`https://yassine-fitness-api.herokuapp.com/date/${workoutDate}`).catch(console.error)
     return data
 }
 export async function listWorkoutsByType(workoutType){
-    const data =  await axios.get(`${URL}/type/${workoutType}`).catch(console.error)
+    const data =  await axios.get(`https://yassine-fitness-api.herokuapp.com/type/${workoutType}`).catch(console.error)
     return data
 }
 export async function readWorkout(workoutId){
-    const data =  await axios.get(`${URL}/workout/${workoutId}`).catch(console.error)
+    const data =  await axios.get(`https://yassine-fitness-api.herokuapp.com/workout/${workoutId}`).catch(console.error)
     return data
 }
 export async function changeWorkout(updatedWorkout){
-    const data =  await axios.put(`${URL}/${updatedWorkout.id}`,updatedNote).catch(console.error)
+    const data =  await axios.put(`https://yassine-fitness-api.herokuapp.com/workout/${updatedWorkout.id}`,updatedNote).catch(console.error)
     return data 
 }
 export async function deleteWorkour(workoutId){
-    await axios.delete(`${URL}/workout/${workoutId}`).catch(console.error)
+    await axios.delete(`https://yassine-fitness-api.herokuapp.com/workout/${workoutId}`).catch(console.error)
 }
