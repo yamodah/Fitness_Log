63c37fee53971df770d6e1482526e5488bb72f25
yamodah <yassinedahlek@gmail.com>
2022-01-08 16:48:20 -0500

    removed repetition stored options in option object

--- App.js
+++ App.js
@@ -9,68 +9,43 @@ import Page3 from "./src/components/Page3";
 import Page4 from "./src/components/Page4";
 import Logs from "./src/components/Logs";
 export default function App() {
+
+  const options={
+    headerStyle: {
+      backgroundColor: '#ef233c99',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    }
+  }
   return (
     <NavigationContainer >
     <Stack.Navigator >
       <Stack.Screen name="Home" component={Home} options={{
           title: 'Home',
-          headerStyle: {
-            backgroundColor: '#ef233c99',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          ...options
+         
         }}/>
       <Stack.Screen name="Logs" component={Logs} options={{
           title: 'Logs',
-          headerStyle: {
-            backgroundColor: '#ef233c99',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          ...options
         }}/>
         <Stack.Screen name="Page1" component={Page1} options={{
           title: 'Page 1',
-          headerStyle: {
-            backgroundColor: '#ef233c99',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          ...options
         }}/>
         <Stack.Screen name="Page2" component={Page2} options={{
           title: 'Page 2',
-          headerStyle: {
-            backgroundColor: '#ef233c99',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          ...options
         }}/>
         <Stack.Screen name="Page3" component={Page3} options={{
           title: 'Page 3',
-          headerStyle: {
-            backgroundColor: '#ef233c99',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          ...options
         }}/>
         <Stack.Screen name="Page4" component={Page4} options={{
           title: 'Page 4',
-          headerStyle: {
-            backgroundColor: '#ef233c99',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          ...options
         }}/>
     </Stack.Navigator>
     </NavigationContainer>
