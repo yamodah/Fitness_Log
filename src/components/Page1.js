a89a9d090a4b6f59c22ca4f07b5c8af22b48e9aa
yamodah <yassinedahlek@gmail.com>
2022-01-09 19:20:24 -0500

    built out majority of page 1 no styling but full functionality

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,37 +1,45 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TextInput,
+  Keyboard
+} from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import { useData } from "./GlobalContext";
 const Page1 = ({ navigation }) => {
   const dataPack = useData();
   const [date, setDate] = useState(new Date());
-  const [type, setType] = useState("");
+  const [type, setType] = useState("Sports");
   const [duration, setDuration] = useState(0);
   const [goal, setGoal] = useState("");
-  const index = ()=>{
-    if(type==="Sports")return 0 
-    if(type==="Resistance")return 1 
-    if(type==="Cardio")return 2
-    return 0
-  }
-  const changeHanlder = (setMethod, value) => {
-    setMethod(value);
-    console.log(value)
+  const index = () => {
+    if (type === "Sports") return 0;
+    if (type === "Resistance") return 1;
+    if (type === "Cardio") return 2;
+    return 0;
   };
-
+  const changeHandler = (setMethod, value) => {
+    setMethod(value);
+    // console.log(typeof value)
+  };
 
   const collectData = () => {
     dataPack.setData({
       date,
       type,
-      duration,
+      duration: Number(duration),
       goal,
       ...dataPack.data,
     });
     navigation.navigate("Page2");
   };
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
     <View style={styles.container}>
       <View style={styles.dateContainer}>
         <Text style={styles.labelText}>Date:</Text>
@@ -40,34 +48,69 @@ const Page1 = ({ navigation }) => {
           value={date}
           mode="date"
           display="compact"
-          onChange={(event, value)=>changeHanlder(setDate,value)}
+          onChange={(event, value) => changeHandler(setDate, value)}
           themeVariant="dark"
         />
       </View>
       <View style={styles.segements}>
         <SegmentedControl
+          style={{ height: 35 }}
           values={["Sports", "Resistance", "Cardio"]}
           selectedIndex={index()}
           backgroundColor="#8d99ae"
           tintColor="#edf2f4"
-          activeFontStyle={{ color: "#ef233c", fontSize: 17 }}
-          onChange={({nativeEvent})=>changeHanlder(setType,nativeEvent.value.toLowerCase())}
+          fontStyle={{ fontSize: 13, color: "#edf2f4" }}
+          activeFontStyle={{ color: "#ef233c", fontSize: 16 }}
+          onChange={({ nativeEvent }) =>
+            changeHandler(setType, nativeEvent.value.toLowerCase())
+          }
         />
       </View>
+      <View style={styles.durationContainer}>
+        <Text style={styles.inputLabel}>Workout Duration:</Text>
+        <TextInput
+          keyboardType="number-pad"
+          onChange={({ nativeEvent }) =>
+            changeHandler(setDuration, nativeEvent.text)
+          }
+          placeholder="min(s)"
+          returnKeyType="done"
+          value={duration}
+        />
+        
+      </View>
+      <View style={styles.goalContainer}>
+        <Text style={styles.inputLabel}>Workout session goal:</Text>
+        <TextInput
+        style={{maxWidth:155, padding:5}}
+          keyboardType="default"
+          onChange={({ nativeEvent }) =>
+            changeHandler(setGoal, nativeEvent.text)
+          }
+          placeholder="general fitness"
+          maxLength={140}
+          returnKeyType="return"
+          value={goal}
+          multiline
+        />
+      </View>
+        <TouchableOpacity onPress={Keyboard.dismiss} accessible={false} style={{marginTop:10}}><View><Text style={styles.buttonText}>Done</Text></View></TouchableOpacity>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={collectData}>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
             navigation.navigate("Home");
           }}
+          
         >
-          <Text style={styles.buttonText}>cancel</Text>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={collectData}>
+          <Text style={styles.buttonText}>Next</Text>
         </TouchableOpacity>
       </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 };
 const styles = StyleSheet.create({
@@ -75,15 +118,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#d90429",
     alignItems: "center",
-    justifyContent: "space-around",
-  },
-  secondaryBG: {
-    width: "85%",
-    flex: 1,
-    // backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    borderRadius: 30,
+    justifyContent: "flex-start",
+    paddingTop:55
   },
   text: {
     color: "#edf2f4",
@@ -106,7 +142,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-
+    marginTop:150
+    // alignSelf:"flex-start"
     //  backgroundColor:"white"
   },
   dateContainer: {
@@ -115,6 +152,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: 250,
     color: "#edf2f4",
+    marginBottom: 10,
   },
   labelText: {
     color: "#edf2f4",
@@ -123,7 +161,41 @@ const styles = StyleSheet.create({
   segements: {
     width: "80%",
     justifyContent: "center",
+    marginBottom: 10,
     // backgroundColor: "#edf2f4",
   },
+  durationContainer: {
+    marginBottom: 15,
+    marginTop:15,
+    borderRadius: 10,
+    minWidth: 20,
+    minHeight: 30,
+    maxWidth:"85%",
+    backgroundColor: "#edf2f4",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingRight: 10,
+    paddingLeft: 10,
+    flexDirection:"row"
+  },
+  goalContainer: {
+    marginBottom: 15,
+    marginTop:15,
+    borderRadius: 10,
+    minWidth: 20,
+    minHeight: 30,
+    maxWidth:"85%",
+    backgroundColor: "#edf2f4",
+    alignItems: "center",
+    justifyContent: "center",
+    padding:15,
+    flexDirection:"row"
+  },
+  inputLabel:{
+    marginRight:10,
+    color:"#d90429",
+    fontSize:15,
+    // alignSelf:"center"
+  }
 });
 export default Page1;
