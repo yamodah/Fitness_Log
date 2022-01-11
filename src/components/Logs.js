e8182101dbf4e1b02aff2fff6358b700fd531e14
yamodah <yassinedahlek@gmail.com>
2022-01-11 16:01:01 -0500

    logs styling

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -10,21 +10,25 @@ const Logs = () => {
 
     const renderItem =({item})=>{
         return(
-        <View style={styles.container}>
-            <Text>workout number:{item.id}</Text>
-            <Text>workout date:{item.date}</Text>
-            <Text>workout type:{item.type}</Text>
-            <Text>workout durration:{item.durration}</Text>
-            <Text>workout goal:{item.goal}</Text>
-            <Text>pre:{item.pre} post:{item.post} intensity:{item.intensity}</Text>
-           {item.type ==="resistance" && <Text>body parts:{item.body_parts}</Text>}
-           {item.type ==="sport" && <View><Text>sport:{item.sport} played at/in:{item.venue}</Text><Text>competition:{item.competion}</Text></View>}
-           {item.type ==="cardio" && <Text>cardio type:{item.cardio_type} speed:{item.speed}</Text>}
+        <View style={styles.cardContainer}>
+            <View style={styles.textGroup}>    
+            <Text style={styles.workoutNum}>ID: {item.id}</Text>
+            <Text style={styles.text}><>DATE:</> {item.date}</Text>
+            <Text style={styles.text}>TYPE: {item.type}</Text>
+            {item.type==="cardio"&& <Text style={styles.text}>DURATION: {item.duration}</Text>}
+            </View>
+            <View style={styles.textGroup}> 
+            <Text style={styles.text}>GOAL: {item.goal}</Text>
+            <Text style={styles.text}>PRE: {item.pre} || POST: {item.post} || INTENSITY: {item.intensity}</Text>
+           {item.type ==="resistance" && <Text style={styles.text}>BODY PARTS: {item.body_parts}</Text>}
+           {item.type ==="sports" && <View><Text style={styles.text}>SPORT: {item.sport} || AT/IN: {item.venue}</Text><Text style={styles.text}>COMPETITION: {item.competion?"YES":"NO"}</Text></View>}
+           {item.type ==="cardio" && <Text style={styles.text}>cardio type: {item.cardio_type} || speed: {item.speed}</Text>}
+           </View>
         </View>
         )
     }
     return (
-    <View>
+    <View style={styles.container}>
             <FlatList
             data={logs}
             renderItem={renderItem}
@@ -42,5 +46,31 @@ const styles = StyleSheet.create({
         backgroundColor: "#d90429",
         alignItems: "center",
         justifyContent: "center",
+        
+        
+    },
+    cardContainer:{
+        backgroundColor: "#edf2f499",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop:15,
+        borderRadius:10,
+        padding:15,
+        width:"100%"
+    },
+    textGroup:{
+        marginBottom:2,
+        marginTop:10,
+        backgroundColor:"#edf2f4",
+        borderRadius:10,
+        padding:10,
+        width:"95%"
+    },
+    workoutNum:{
+        color:"#d90429"
+    },
+    text:{
+        marginTop:2
     }
+
 })
