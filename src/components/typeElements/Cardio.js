3e155dded7bbb74db0d9f6bd147e5934670f7ae4
yamodah <yassinedahlek@gmail.com>
2022-01-28 11:24:52 -0500

    added rnfes to each file

---
+++ src/components/typeElements/Cardio.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Cardio = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Cardio;
+
+const styles = StyleSheet.create({});
---
+++ src/components/typeElements/Resistance.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Resistance = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Resistance;
+
+const styles = StyleSheet.create({});
