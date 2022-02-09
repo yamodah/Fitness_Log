548f37190447b8cc9da4177e6aa4feab3075ad41
yamodah <yassinedahlek@gmail.com>
2022-02-08 20:41:11 -0500

    added SelectMultipleGroupButton and dependency builidng out resistance page

--- src/components/typeElements/Resistance.js
+++ src/components/typeElements/Resistance.js
@@ -1,18 +1,45 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import SelectMultipleGroupButton from 'react-native-selectmultiple-button/libraries/SelectMultipleGroupButton';
 //what body parts
     /*
     buttons that when pressed populate a string or 
     */
 //make  a set that holds body part names 
-const Resistance = () => {
+const Resistance = ({navigation}) => {
+  const [bodyParts, setBodyParts] = useState([])
+  const bodyPartOptions = {
+    value:"Biceps",
+    value:"Triceps",
+    value:"Hamstrings",
+    value:"Quads",
+    value:"Glute",
+    value:"Shoulders",
+    value:"Upper Back",
+    value:"Lower Back",
+    value:"Core",
+    value:"Full-Body",
+  }
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text>I worked out {bodyParts.join("")}</Text>
+      <SelectMultipleGroupButton
+      group={bodyPartOptions}
+      
+      />
     </View>
   );
 };
 
 export default Resistance;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d90429",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop:100,
+    paddingBottom:150
+  }
+});
