51f22820bb8785c00a5b53f95049b21e32551a3b
yamodah <yassinedahlek@gmail.com>
2022-02-08 21:28:33 -0500

    added labels as emojis to sliders

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,81 +1,115 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import Slider from "@react-native-community/slider";
 import { useData } from "./GlobalContext";
 const Page2 = ({ navigation, route }) => {
   const [pre, setPre] = useState(0);
   const [post, setPost] = useState(0);
   const [intensity, setIntensity] = useState(0);
-  const dataPack= useData()
+  const dataPack = useData();
 
   const changeHandler = (setMethod, value) => {
     setMethod(value);
   };
-  const collectData = ()=>{
+  const collectData = () => {
     dataPack.setData({
       ...dataPack.data,
-      pre,post,intensity,
-    })
-    navigation.navigate("Page3")
-  }
+      pre,
+      post,
+      intensity,
+    });
+    navigation.navigate("Page3");
+  };
   // console.log(dataPack.data)
   return (
     <View style={styles.container}>
       <View style={styles.slidersContainer}>
-      <View style={styles.sliderContainer}>
-      <Text style={styles.sliderText}>
-      Pre exercise  
-      </Text>
-      <Slider
-        style={{ width: 245, height: 40 }}
-        minimumValue={0}
-        maximumValue={100}
-        minimumTrackTintColor="#d90429"
-        maximumTrackTintColor="#2b2d42"
-        tapToSeek={true}
-        step={1}
-        onSlidingComplete={(val)=>changeHandler(setPre,val)}
-      />
-      </View>
-      <View style={styles.sliderContainer}>
-      <Text style={styles.sliderText}>
-      Post exercise 
-      </Text>
-      <Slider
-        style={{ width: 245, height: 40 }}
-        minimumValue={0}
-        maximumValue={100}
-        minimumTrackTintColor="#d90429"
-        maximumTrackTintColor="#2b2d42"
-        tapToSeek={true}
-        step={1}
-        onSlidingComplete={(val)=>changeHandler(setPost,val)}
-      />
-      </View>
-      <View style={styles.sliderContainer}>
-      <Text style={styles.sliderText}>
-       General intensity
-      </Text>
-      <Slider
-        style={{ width: 245, height: 40 }}
-        minimumValue={0}
-        maximumValue={100}
-        minimumTrackTintColor="#d90429"
-        maximumTrackTintColor="#2b2d42"
-        tapToSeek={true}
-        step={1}
-        onSlidingComplete={(val)=>changeHandler(setIntensity,val)}
-        />
+        <View style={styles.sliderContainer}>
+          <Text style={styles.sliderText}>Pre exercise</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: 260,
+            }}
+          >
+            <Text style={{fontSize:20}}>😐</Text>
+            <Text style={{fontSize:20}}>🤪</Text>
+          </View>
+          <Slider
+            style={{ width: 245, height: 40 }}
+            minimumValue={0}
+            maximumValue={100}
+            minimumTrackTintColor="#d90429"
+            maximumTrackTintColor="#2b2d42"
+            tapToSeek={true}
+            step={1}
+            onSlidingComplete={(val) => changeHandler(setPre, val)}
+          />
         </View>
-       </View> 
+        <View style={styles.sliderContainer}>
+          <Text style={styles.sliderText}>Post exercise</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: 260,
+            }}
+          >
+            <Text style={{fontSize:20}}>👎</Text>
+            <Text style={{fontSize:20}}>💪</Text>
+          </View>
+          <Slider
+            style={{ width: 245, height: 40 }}
+            minimumValue={0}
+            maximumValue={100}
+            minimumTrackTintColor="#d90429"
+            maximumTrackTintColor="#2b2d42"
+            tapToSeek={true}
+            step={1}
+            onSlidingComplete={(val) => changeHandler(setPost, val)}
+          />
+        </View>
+        <View style={styles.sliderContainer}>
+          <Text style={styles.sliderText}>General intensity</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: 260,
+            }}
+          >
+            <Text style={{fontSize:20}}>😴</Text>
+            <Text style={{fontSize:20}}>🥵</Text>
+          </View>
+          <Slider
+            style={{ width: 245, height: 40 }}
+            minimumValue={0}
+            maximumValue={100}
+            minimumTrackTintColor="#d90429"
+            maximumTrackTintColor="#2b2d42"
+            tapToSeek={true}
+            step={1}
+            onSlidingComplete={(val) => changeHandler(setIntensity, val)}
+          />
+        </View>
+      </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={()=>{navigation.goBack()}}>
-              <Text style={styles.buttonText}>Go back</Text>
-            </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={collectData}
+          onPress={() => {
+            navigation.goBack();
+          }}
         >
+          <Text style={styles.buttonText}>Go back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={collectData}>
           <Text style={styles.buttonText}>Next</Text>
         </TouchableOpacity>
       </View>
@@ -88,8 +122,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#d90429",
     alignItems: "center",
     justifyContent: "space-between",
-    paddingTop:90,
-    paddingBottom:60
+    paddingTop: 90,
+    paddingBottom: 60,
   },
   button: {
     //   backgroundColor: "#edf2f499",
@@ -105,36 +139,36 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: "row",
-    alignSelf:"center",
-    marginBottom:75
+    alignSelf: "center",
+    marginBottom: 75,
 
     //  backgroundColor:"white"
   },
-  sliderContainer:{
-    flexDirection:"column",
-    justifyContent:"center",
-    width:350,
-    margin:15,
-    padding:5,
-    alignItems:"center",
-
+  sliderContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    width: 350,
+    margin: 15,
+    padding: 5,
+    alignItems: "center",
+    marginBottom:0
   },
-  sliderText:{
-    color:"#d90429",
-    fontSize:18,
-    alignSelf:"center",
-    marginBottom:5,
-    marginLeft:10
+  sliderText: {
+    color: "#d90429",
+    fontSize: 18,
+    alignSelf: "center",
+    marginBottom: 5,
+    marginLeft: 10,
   },
-  slidersContainer:{
-  margin:5,
-  backgroundColor:"#edf2f4",
-  //d90429
-  borderRadius:15,
-  maxWidth:"85%",
-  justifyContent:"center",
-  alignItems:"center",
-  paddingBottom:15
-  }
+  slidersContainer: {
+    margin: 5,
+    backgroundColor: "#edf2f4",
+    //d90429
+    borderRadius: 15,
+    maxWidth: "85%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 15,
+  },
 });
 export default Page2;
