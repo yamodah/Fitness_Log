7725206d93e9cc8fd5aa357bf2a29654d9b85a4b
yamodah <yassinedahlek@gmail.com>
2022-02-09 12:59:53 -0500

    added modal to page 4 and passed dwon props to components

--- src/components/Page4.js
+++ src/components/Page4.js
@@ -1,17 +1,39 @@
-import React from 'react'
-import { StyleSheet, View} from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, View, Modal, TouchableOpacity} from 'react-native'
 import Cardio from './typeElements/Cardio';
 import Sports from './typeElements/Sports';
 import Resistance from './typeElements/Resistance';
+import { useData } from './GlobalContext';
 const Page4 = ({navigation,route}) => {
-
+  const [visible,setVisible]=useState(false)
+  const dataPack = useData()
   const {type}= route.params
     return (
         <View style={styles.container}>
-          {type==="cardio" && <Cardio navigation={navigation}/>}
-          {type==="sports" && <Sports navigation={navigation}/>}
-          {type==="resistance" && <Resistance navigation={navigation}/>}
+          {type==="cardio" && <Cardio navigation={navigation} modalToggle={setVisible}/>}
+          {type==="sports" && <Sports navigation={navigation} modalToggle={setVisible}/>}
+          {type==="resistance" && <Resistance navigation={navigation} modalToggle={setVisible}/>}
+          <Modal
+          animationType='slide'
+          visible={visible}
+          >
+            <Text>{dataPack.data}</Text>
+          <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Text style={styles.buttonText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={collectData}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
         </View>
+          </Modal>
+        </View>
+        
     )
 }
 const styles = StyleSheet.create({
